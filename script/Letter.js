93a535db315b64a3718bea32d86a45591448c1f5
Ivan <emelianov.i.a17@gmail.com>
2020-12-19 12:07:26 +0800

    feat: Инициализирование писем

---
+++ script/Letter.js
@@ -0,0 +1,78 @@
+function Letter(id = 0, name = '', surname = '', email = '', text = '') {
+  this.id = id;
+  this.name = name;
+  this.surname = surname;
+  this.email = email;
+  this.text = text;
+}
+
+let mailbox;
+
+const initialSenders = [{
+  name: 'Левин',
+  surname: 'Маркович',
+  email: 'aojv@mail.ru',
+},{
+  name: 'Ольга',
+  surname: 'Григорьева',
+  email: 'kggfpxw@yandex.ru',
+},{
+  name: 'Надежда',
+  surname: 'Смирнова',
+  email: 'oxxv@yandex.ru',
+},{
+  name: 'Давид',
+  surname: 'Андрианов',
+  email: 'f9jxjd14@gmail.com',
+},{
+  name: 'Ярослава',
+  surname: 'Астафьева',
+  email: 'p24a@mail.ru',
+},{
+  name: 'Ярослав',
+  surname: 'Маслов',
+  email: 'ahbg@yandex.ru',
+},{
+  name: 'Анастасия',
+  surname: 'Панова',
+  email: 'xl9bc5@gmail.com',
+},{
+  name: 'Михаил',
+  surname: 'Беляков',
+  email: 'n4zc9kz@yandex.ru',
+},{
+  name: 'Максим',
+  surname: 'Казанцев',
+  email: 'copaa6@gmail.com',
+},{
+  name: 'Арина',
+  surname: 'Громова',
+  email: 'myrfqpb@mail.ru',
+}];
+
+function getRandInt(min, max) {
+  return Math.floor(min - (min - max -1) * Math.random());
+}
+
+function addLetter(id, sender, text) {
+  return new Letter(id, sender.name, sender.surname, sender.email, text)
+}
+
+function initialLetters(initialSenders, proseArray) {
+  return initialSenders.map( (sender, id) => {  let randomIntIndexProse = getRandInt(0, proseArray.length - 1);
+                                                return addLetter(id, sender, proseArray[randomIntIndexProse].fields.text); });
+}
+
+function loadPoems() {
+  url = "https://raw.githubusercontent.com/IlyaGusev/PoetryCorpus/master/datasets/django/all_django.json";
+  
+  fetch(url)
+    .then(respone => respone.json())
+    .then(proseArray => {
+      mailbox = initialLetters(initialSenders, proseArray);
+      console.log(mailbox);
+    })
+    .catch(error => console.log(error));
+}
+
+loadPoems();
